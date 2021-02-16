6140fd3123437c6113173e40c40d208fafe31676
Joe <josephedwardwork@gmail.com>
2021-02-16 00:18:55 -0500

    Create heroku_redeploy.sh

---
+++ heroku_redeploy_apps/heroku_redeploy.sh
@@ -0,0 +1,13 @@
+# heroku login 
+# heroku plugins:install heroku-releases-retry
+apps=$(heroku apps) 
+IFS=" "
+apps1=($apps)
+IFS="
+"
+appList=(${apps1[@]:2})
+set +e
+for x in ${appList[@]};
+    do
+    heroku releases:retry --app $x
+    done
