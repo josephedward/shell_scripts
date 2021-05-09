7d45a1f1ae7e2c116716c9ec5e84b017f67b9418
Joe <josephedwardwork@gmail.com>
2021-05-09 08:08:21 -0400

    add scripts

--- mern_preflight.sh
+++ mern_preflight.sh
@@ -1,14 +1,15 @@
+# 
+sudo su
+
+
 brew services start mongodb-community
 echo 'mongo started'
 
-# prevents 
-# sudo su
-
 echo 'attempting to close ports'
-sudo killall -9 node
+killall -9 node
 echo 'ports test:'
-sudo lsof -i tcp:8887 
-sudo lsof -i tcp:8888 
+lsof -i tcp:8887 
+lsof -i tcp:8888 
 
 
 
