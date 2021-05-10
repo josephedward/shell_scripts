47b4fbd3e3ead53b89b10ac29d94426ae413dce5
josephedward <josephedwardwork@gmail.com>
2021-05-10 12:26:18 -0400

    commit

--- linux/chart_quick_install-linux.sh
+++ linux/chart_quick_install-linux.sh
@@ -14,7 +14,18 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get install docker docker-engine docker.io containerd runc
 docker run hello-world #verifies progress
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-install minikube-linux-amd64 /usr/local/bin/minikube
-minikube start
-# INCOMPLETE 
+curl -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash
+# in case it doesn't take
+echo 'K3D_HOME=/home/linuxbrew/.linuxbrew/Cellar/k3d/4.4.3'>> .bashrc
+echo 'PATH=$PATH:$K3D_HOME/bin'>> .bashrc
+k3d cluster create mycluster
+k3d kubeconfig merge mycluster --kubeconfig-switch-context
+# verify
+kubectl get nodes
+# install helm
+curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+helm version
+helm repo add verdaccio https://charts.verdaccio.org
+helm repo update
+# helm install verdaccio-deployment-1 verdaccio/verdaccio
+echo $(helm install verdaccio-deployment-1 verdaccio/verdaccio) > verdaccio-deployment-1.info
--- remove_from_git.sh
+++ remove_from_git.sh
@@ -1,4 +1,3 @@
-cd $1
 git add . && git commit -m "commit"
-git filter-branch --index-filter "git rm -rf --cached --ignore-unmatch ${$2}" HEAD
+git filter-branch --index-filter "git rm -rf --cached --ignore-unmatch ${$1}" HEAD
 git push -f origin master
