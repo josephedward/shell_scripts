c7ddb45227b78e1967aec2c943bdb889c604d8de
josephedward <josephedwardwork@gmail.com>
2021-05-10 13:01:19 -0400

    Update chart_quick_install-linux.sh

--- linux/chart_quick_install-linux.sh
+++ linux/chart_quick_install-linux.sh
@@ -24,8 +24,13 @@ k3d kubeconfig merge mycluster --kubeconfig-switch-context
 kubectl get nodes
 # install helm
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+# verify
 helm version
 helm repo add verdaccio https://charts.verdaccio.org
 helm repo update
 # helm install verdaccio-deployment-1 verdaccio/verdaccio
 echo $(helm install verdaccio-deployment-1 verdaccio/verdaccio) > verdaccio-deployment-1.info
+export POD_NAME=$(kubectl get pods --namespace default -l "app.kubernetes.io/name=verdaccio,app.kubernetes.io/instance=verdaccio-deployment-1" -o jsonpath="{.items[0].metadata.name}")
+export CONTAINER_PORT=$(kubectl get pod --namespace default $POD_NAME -o jsonpath="{.spec.containers[0].ports[0].containerPort}")
+echo "Visit http://127.0.0.1:8080 to use your application"
+kubectl --namespace default port-forward $POD_NAME 8080:$CONTAINER_PORT
