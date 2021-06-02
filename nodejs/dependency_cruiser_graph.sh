b45ee07cd9b1ff0c062f9e81e296acd6853a2fd2
josephedward <josephedwardwork@gmail.com>
2021-06-02 18:08:32 -0400

    commit

--- nodejs/dependency_cruiser_graph.sh
+++ nodejs/dependency_cruiser_graph.sh
@@ -4,5 +4,5 @@
 #as validator: 
 # npm install --save-dev dependency-cruiser
  
-# argument is root folder of project you want to analyze 
+# 1st argument is build modules to exclude, second is root folder of project you want to analyze 
 depcruise --exclude $1 --output-type dot $2 | dot -T svg > dependencygraph.svg
