fca3d30b3f4a215068a3cebdb2bca0685bbfa2ac
josephedward <josephedwardwork@gmail.com>
2025-09-23 12:23:27 -0400

    Delete remove_from_git.sh

--- git/remove_from_git.sh
+++
@@ -1,6 +0,0 @@
-git add . && git commit -m "commit"
-git filter-branch -f --index-filter "git rm -rf --cached --ignore-unmatch $1" HEAD
-git push -f origin main
-
-
-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
